2b2997b104eca07944efdbecb74a4e5a24060feb
Goran Petrovic <stgpetrovic@gmail.com>
2023-10-20 16:33:21 +0200

    Clean up the script, use cuda

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from encdec import Tokenizer
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
+# hyperparameters
+block_size = 8 # max length to send for training, context_length
+batch_size = 32 # how many we can batch together for performance
+max_iters = 3000
+eval_interval = 300
+lr = 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+
 torch.manual_seed(1337)
 
 def ReadInput(f):
@@ -18,11 +28,12 @@ def Split(data, ratio):
     n = int(ratio*len(data))
     return data[:n], data[n:]
 
-def GetBatch(data, batch_size, block_size):
+def GetBatch(data, split):
     # batch contans data of inputs x and targets y
-    ix = torch.randint(high=len(data) - block_size, size=(batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:1+i+block_size] for i in ix])
+    ix = torch.randint(high=len(data[split]) - block_size, size=(batch_size,))
+    x = torch.stack([data[split][i:i+block_size] for i in ix])
+    y = torch.stack([data[split][i+1:1+i+block_size] for i in ix])
+    x, y = x.to(device), y.to(device)
     return x, y
 
 class BigramLanguageModel(nn.Module):
@@ -48,37 +59,51 @@ class BigramLanguageModel(nn.Module):
             logits, loss = self(idx)
             logits = logits[:, -1, :] # drop T, get BC
             # get probabilities
-            probs = F.softmax(logits, dim=1) # BC
+            probs = F.softmax(logits, dim=-1) # BC
             # sample from distribution
             idx_next = torch.multinomial(probs, num_samples=1) # B,1
             # append samplex index to running sequence
             idx = torch.cat((idx, idx_next), dim=1) # B,T+1
         return idx
 
-def TryBigramLM(tknz, data, batch_size, block_size, xb, yb):
-    m = BigramLanguageModel(len(tknz))
-    logits, loss = m(xb, yb)
-    idx = torch.zeros((1,1), dtype=torch.long)
-    print(tknz.decode(m.generate(idx, max_new_tokens=100)[0].tolist()))
+def TryBigramLM(tknz, data, xb, yb):
+    model = BigramLanguageModel(len(tknz))
+    m = model.to(device)
+    Train(tknz, model, data, 'train')
 
-    # more serious attempt
-    Train(tknz, m, data, batch_size, block_size, 10000)
+@torch.no_grad()
+def EstimateLoss(model, data):
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters, )
+        for k in range(eval_iters):
+            x, y = GetBatch(data, split)
+            logits, loss = model(x, y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
-def Train(tknz, m, data, batch_size, block_size, steps):
- optimizer = torch.optim.AdamW(m.parameters(), lr=1e-3)
- for s in range(steps):
+def Train(tknz, model, data, split):
+ optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+ for s in range(max_iters):
+    if s % eval_interval == 0:
+        losses = EstimateLoss(model, data)
+        print(f"step {s}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
     # sample
-    xb, yb = GetBatch(data, batch_size, block_size)
+    xb, yb = GetBatch(data, split)
 
     # evaluate loss
-    logits, loss = m(xb, yb)
+    logits, loss = model(xb, yb)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
 
  print(loss.item())
- idx = torch.zeros((1,1), dtype=torch.long)
- print(tknz.decode(m.generate(idx, max_new_tokens=500)[0].tolist()))
+ context = torch.zeros((1,1), dtype=torch.long, device=device)
+ print(tknz.decode(model.generate(context, max_new_tokens=500)[0].tolist()))
 
 def Main():
     text = ReadInput('input/input.txt')
@@ -88,9 +113,9 @@ def Main():
     data = torch.tensor(tknz.encode(text), dtype=torch.long)
 
     train_data, val_data = Split(data, 0.9)
+    data = {'train': train_data, 'val': val_data}
     print('train size %d, validation size %d' % (len(train_data), len(val_data)))
 
-    block_size = 8 # max length to send for training, context_length
     x = train_data[:block_size]
     y = train_data[:block_size+1]
     for t in range(block_size):
@@ -98,8 +123,7 @@ def Main():
         target = y[t]
         print(f"when input is {context} target is {target}")
 
-    batch_size = 32 # how many we can batch together for performance
-    xb, yb = GetBatch(train_data, batch_size, block_size)
+    xb, yb = GetBatch(data, 'train')
     print (f"shapes {x.shape} {y.shape}")
     print (xb)
 
@@ -108,7 +132,7 @@ def Main():
             context = xb[b, :t+1]
             target = yb[b, t]
             print(f"when input is {context.tolist()} the target: {target}")
-    TryBigramLM(tknz, train_data, batch_size, block_size, xb, yb)
+    TryBigramLM(tknz, data, xb, yb)
 
 
 
