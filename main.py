63f9d4ce7fcfd50a95bb19cb032a669c83d44c9f
Goran Petrovic <stgpetrovic@gmail.com>
2023-10-20 18:45:59 +0200

    scale up

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-block_size = 8 # max length to send for training, context_length
-batch_size = 32 # how many we can batch together for performance
-max_iters = 3000
-eval_interval = 300
-lr = 1e-2
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+block_size = 256 # max length to send for training, context_length
+batch_size = 64 # how many we can batch together for performance
+max_iters = 5000
+eval_interval = 500
+lr = 3e-4
+device = 'cuda' # if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 32
+n_embd = 384
+n_layer = 6
+n_head = 6
+dropout = .2
 
 torch.manual_seed(1337)
 
@@ -50,6 +53,7 @@ class Head(nn.Module):
         self.query = nn.Linear(n_embd, head_size, bias=False)
         self.value = nn.Linear(n_embd, head_size, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
+        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         B, T, C = x.shape
@@ -58,9 +62,57 @@ class Head(nn.Module):
         w = q @ k.transpose(-2, -1) * C**-.5 # BTC @ BCT => BTT
         w = w.masked_fill(self.tril[:T,:T] == 0, float('-inf')) #BTT
         w = F.softmax(w, dim=-1) # BTT
+        w = self.dropout(w) #BTT
         v = self.value(x) # BTC
         out = w @ v # BTT @ BTC => BTC
         return out
+
+class FeedForward(nn.Module):
+    """simple linear layer followed by nonlinearity"""
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+                nn.Linear(n_embd, 4 * n_embd),
+                nn.ReLU(),
+                nn.Linear(4 * n_embd, n_embd),
+                nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class MultiHeadAttention(nn.Module):
+    """multiple egos"""
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(n_embd, n_embd)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+
+class Block(nn.Module):
+    """transformer block: talk then crunch"""
+
+    def __init__(self, n_embd, n_head):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embd)
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+        return x
 
 
 class BigramLanguageModel(nn.Module):
@@ -69,7 +121,8 @@ class BigramLanguageModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(len(tknz), n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
-        self.sa_head = Head(n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head=n_head) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
         self.lm_head = nn.Linear(n_embd, len(tknz))
 
     def forward(self, idx, targets=None):
@@ -78,7 +131,8 @@ class BigramLanguageModel(nn.Module):
         tok_emb = self.token_embedding_table(idx) #BTC
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) # T C
         x = tok_emb + pos_emb # BTC
-        x = self.sa_head(x) #BTC
+        x = self.blocks(x) #BTC
+        x = self.ln_f(x) #BTC
         logits = self.lm_head(x)  #B T vocab_size
 
         if targets is None:
@@ -140,7 +194,7 @@ def Train(tknz, model, data, split):
 
  print(loss.item())
  context = torch.zeros((1,1), dtype=torch.long, device=device)
- print(tknz.decode(model.generate(context, max_new_tokens=500)[0].tolist()))
+ print(tknz.decode(model.generate(context, max_new_tokens=10000)[0].tolist()))
 
 def Main():
 
